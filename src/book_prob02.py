ddc3842fb4bbf6edd837d741a90c2542cb4ebe2e
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2015-01-19 19:23:39 -0500

    Make sample plot more useable
    Now the sample plot script checks whether a filename was provided on the
    command line, and if not, shows the figure with pyplot; if a filename is
    provided, the ordinary behavior (printing figure to filename) prevails.
    The printed figure now uses tex for rendering, and even a brief preamble to
    make the typeface match the rest of the document.

--- src/book_prob02.py
+++ src/book_prob02.py
@@ -3,7 +3,7 @@ import sys
 import numpy as np
 
 import matplotlib as mlib
-mlib.use('Agg')
+# mlib.use('Agg')
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -19,32 +19,53 @@ def func_finite_c(x):
 def func_finite_d(x):
     return x * np.log(x**2)
 
-fig = Figure(figsize=(6,6))
-canvas = FigureCanvas(fig)
-ax_a = fig.add_subplot(2,2,1)
-cobweb_plot(ax_a, func_finite_a, 0.05, np.linspace(-0.1, 0.1, 500),
-        n_iter=50)
-ax_a.set_xlim(-0.1, .10)
-ax_a.set_title('a: $x_{n+1} = 0.5 x_n (1 - x_n)$')
-ax_b = fig.add_subplot(2,2,2)
-cobweb_plot(ax_b, func_finite_b, 1.62, np.linspace(1, 2, 500),
-        n_iter=5)
-ax_b.set_xlim(1.4, 2)
-ax_b.set_title('b: $x_{n+1} = -x_n^2 (1-x_n)$')
-ax_c = fig.add_subplot(2,2,3)
-cobweb_plot(ax_c, func_finite_c, 0.7, np.linspace(0.2, 0.75, 500),
-        n_iter=50)
-ax_c.set_title('c: $x_{n+1} = 1/(2+x_n)$')
-ax_d = fig.add_subplot(2,2,4)
-cobweb_plot(ax_d, func_finite_d, 2, np.linspace(0.1, 4, 500),
-        n_iter=4)
-ax_d.set_xlim(1, 4)
-ax_d.set_title('d: $x_{n+1} = x_n \mathrm{ln}x_n^2$')
-for ax in [ax_a, ax_b, ax_c, ax_d]:
-    ax.grid()
-    ax.set_xlabel('$x_n$')
-    ax.set_ylabel('$x_{n+1}$')
+def plot_analysis(fname=None):
+    if not fname:
+        # if no file name provided, show figure with pyplot
+        fig = plt.figure()
+    else:
+        # if a file name is provided, set up canvas to print figure
+        fig = Figure(figsize=(6,6))
+        canvas = FigureCanvas(fig)
+        pass
+    ax_a = fig.add_subplot(2,2,1)
+    cobweb_plot(ax_a, func_finite_a, 0.05, np.linspace(-0.1, 0.1, 500),
+            n_iter=50)
+    ax_a.set_xlim(-0.1, .10)
+    ax_a.set_title('a: $x_{n+1} = 0.5 x_n (1 - x_n)$')
+    ax_b = fig.add_subplot(2,2,2)
+    cobweb_plot(ax_b, func_finite_b, 1.62, np.linspace(1, 2, 500),
+            n_iter=5)
+    ax_b.set_xlim(1.4, 2)
+    ax_b.set_title('b: $x_{n+1} = -x_n^2 (1-x_n)$')
+    ax_c = fig.add_subplot(2,2,3)
+    cobweb_plot(ax_c, func_finite_c, 0.7, np.linspace(0.2, 0.75, 500),
+            n_iter=50)
+    ax_c.set_title('c: $x_{n+1} = 1/(2+x_n)$')
+    ax_d = fig.add_subplot(2,2,4)
+    cobweb_plot(ax_d, func_finite_d, 2, np.linspace(0.1, 4, 500),
+            n_iter=4)
+    ax_d.set_xlim(1, 4)
+    ax_d.set_title('d: $x_{n+1} = x_n \mathrm{ln}x_n^2$')
+    for ax in [ax_a, ax_b, ax_c, ax_d]:
+        ax.grid()
+        ax.set_xlabel('$x_n$')
+        ax.set_ylabel('$x_{n+1}$')
 
-fig.tight_layout()
+    if not fname:
+        # if no file name provided, show figure with pyplot
+        plt.show()
+    else:
+        # if a file name is provided, pretty print all using tex, and render
+        # to file
+        mlib.rcParams['text.usetex'] = True
+        mlib.rcParams['text.latex.preamble'] = '\usepackage{libertine}'
+        fig.tight_layout()
+        canvas.print_figure(fname)
 
-canvas.print_figure(sys.argv[1])
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        fname = sys.argv[1]
+    else:
+        fname = None
+    plot_analysis(fname)
